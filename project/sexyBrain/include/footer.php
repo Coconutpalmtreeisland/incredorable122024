c44d6d22ae4aef873251463e95987eae684e2e46
coconutpalmtreeisland <adorableness1215@naver.com>
2023-10-26 17:35:45 +0900

    23.10.26 수업 추가

---
+++ project/sexyBrain/include/footer.php
@@ -0,0 +1,15 @@
+<footer id="footer">
+    <div class="footer__logo">
+        <a href="home.html"><img src="assets/img/footer_logo.png" alt="뇌섹남녀" href></a>
+    </div>
+    <div class="footer__info">
+        <ul class="footer__creater">
+            <li>권초록</li>
+            <li>오종석</li>
+            <li>이원영</li>
+            <li>이혜민</li>
+        </ul>
+        <div class="footer__copyright">© 2023 뇌섹남녀</div>
+    </div>
+</footer>
+<!-- //footer -->
